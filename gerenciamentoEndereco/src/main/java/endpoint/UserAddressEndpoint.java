f91104f060c892a16ea4f0ea59c7d4f59ed08c2f
IagoSMagalhaes <40207586+IagoSMagalhaes@users.noreply.github.com>
2018-09-10 11:20:36 -0300

    Update UserAddressEndpoint.java

--- gerenciamentoEndereco/src/main/java/endpoint/UserAddressEndpoint.java
+++ gerenciamentoEndereco/src/main/java/endpoint/UserAddressEndpoint.java
@@ -71,7 +71,7 @@ public class UserAddressEndpoint {
 
 	@PutMapping(path="/update")
 	@ApiOperation(value = "Update userAddress" , response = String.class)
-	public ResponseEntity<?> updateUserAddres(@Valid @RequestBody UserAddress userAddress){		
+	public ResponseEntity<?> update(@Valid @RequestBody UserAddress userAddress){		
 		verifyIfUserAddressExist(userAddress.getId());		
 		userAddressDAO.save(userAddress);
 		return new ResponseEntity<>(null, HttpStatus.OK);
